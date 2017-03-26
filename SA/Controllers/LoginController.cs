737d1faaeacb448979a7290fd33a79e5f2f72a21
Daniel <daniel.pitthansilveira@gmail.com>
2017-03-26 20:01:46 -0300

    Ajax Funcionando

--- SA/Controllers/LoginController.cs
+++ SA/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
 ﻿using SA.DAO;
+using SA.Models;
 using SA.ViewModel;
 using System;
 using System.Collections.Generic;
@@ -31,21 +32,44 @@ namespace SA.Controllers
         /// <param name="cpf"></param>
         /// <returns></returns>
         [HttpPost]
-        public ActionResult ValidarLogin(string numCPF)
-        {
-            Login login = new Login();
-            login.Cpf = numCPF;
-            login.Valido =  loginDAO.ValidaLogin(login);         
-
+        public ActionResult ValidarLogin(Login login)
+        { 
+           login.Valido =  loginDAO.ValidaLogin(login);         
 
             if (!login.Valido)
             {
                 //Se o login não é válido, retorna mensagem, ou algo assim
-                return View("Index", login);
+                return PartialView("Login/_Login", login);
             }
-
             //Se tudo deu certo, redireciona para a Home do Caboclo
             return RedirectToAction("Index", "Home");
         }
+
+        /// <summary>
+        /// Formulário de criação de Login
+        /// </summary>
+        /// <returns>ActionResult.</returns>
+        public ActionResult NovoLogin()
+        {
+
+            //ViewBag.Usuarios = this.usuarioDAO.Lista();
+            return View();
+        }
+
+        public ActionResult Add(Usuario user)
+        {
+            return RedirectToAction("ListaUsuarios", "Login");
+        }
+
+        /// <summary>
+        /// Lista os usuários 
+        /// </summary>
+        /// <returns>ActionResult.</returns>
+        public ActionResult ListaUsuarios()
+        {
+            return View();
+        }
+
+
     }
 }
