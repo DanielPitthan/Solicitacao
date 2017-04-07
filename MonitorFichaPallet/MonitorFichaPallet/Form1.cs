07482db7ccde3f1d2de87804998804df8657e811
NerdDPS <daniel.pitthansilveira@gmail.com>
2017-04-07 14:59:36 -0300

    Finalizado

--- MonitorFichaPallet/MonitorFichaPallet/Form1.cs
+++ MonitorFichaPallet/MonitorFichaPallet/Form1.cs
@@ -7,35 +7,177 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MonitorFichaPallet.Helpers;
+using System.Threading;
 
 namespace MonitorFichaPallet
 {
     public partial class Form1 : Form
     {
+        public BindingSource binding = new BindingSource();
+        public Font oFontg = new Font(new FontFamily("Calibri"), 14);
+        public Font oFonth = new Font(new FontFamily("Calibri"), 14, FontStyle.Bold);
+        public CancellationTokenSource tcancel = new CancellationTokenSource();
+        public int time = 60000;
+
+        string query = "SELECT	TOP 300 ZD3_STATUS  as [Status],	    Cast(ZD3_EMISSA as Date)  as [Emissão],	        ZD3_HORA    as [Hora]," +
+                                 " ZD3_SEQ           as [Sequência],	    ZD3_COD     as [Produto],	        ZD3_OP      as [OP]," +
+                                 " ZD3_LOTECT        as [Lote], 	        ZD3_NFICHA  as [Ficha Pallet],	    ZD3_CARGA   as [Carga]," +
+                                 " ZD3_NF            as [Nota Fiscal],   ZD3_QTDLID  as [Quantidade Lida],	ZD3_QUANT   as [Quantidade Apontada]" +
+                                 " FROM  ZD3010" +
+                                 " WHERE D_E_L_E_T_ = ''";
+
         public Form1()
-        {
+        {    
             InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            DataGridViewColumn ZD3_STATUS = new DataGridViewColumn(new DataGridViewTextBoxCell());
-            DataGridViewColumn ZD3_CARGA = new DataGridViewColumn(new DataGridViewTextBoxCell());
-            DataGridViewColumn ZD3_OP = new DataGridViewColumn(new DataGridViewTextBoxCell());
-            DataGridViewColumn ZD3_COD = new DataGridViewColumn(new DataGridViewTextBoxCell());
-            DataGridViewColumn ZD3_QUANT = new DataGridViewColumn(new DataGridViewTextBoxCell());
 
-            ZD3_STATUS.HeaderText = "Id";
-            ZD3_STATUS.Name = "id";
-            ZD3_STATUS.Width = 50;
-            ZD3_STATUS.ReadOnly = true;
+            dgMonitor.ColumnHeadersDefaultCellStyle.Font = oFonth;
+            
 
-            dgMonitor.Columns.Add(ZD3_STATUS);
-            dgMonitor.Columns.Add(ZD3_CARGA);
-            dgMonitor.Columns.Add(ZD3_OP);
-            dgMonitor.Columns.Add(ZD3_COD);
-            dgMonitor.Columns.Add(ZD3_QUANT);
-            dgMonitor.Rows[1].Cells["ZD3_STATUS"].Style.BackColor = Color.Yellow;
+            CarregaDados();
+            CarregaDadosasync();
+            Tempoasync();
+          
+
+        }
+
+        private async void Tempoasync()
+        {
+           
+
+
+            while (true)
+            {
+                //Inicia uma  Tarefa
+                try
+                {
+                    await Task.Delay(60, tcancel.Token);
+                }
+                catch (TaskCanceledException ex)
+                {
+                    //Verifica se a task foi cancelada 
+                    if (tcancel.IsCancellationRequested)
+                    {
+                        tcancel.Dispose();
+                        tcancel = new CancellationTokenSource();
+                        Thread.Sleep(2000); //Da um tempinho para parar a thread, dá só uma seguradinha fera                    
+                    }
+                }
+
+                
+                lbtime.Text = Convert.ToString(DateTime.Now);
+            }
+        }
+
+
+
+        private async void CarregaDadosasync()
+        {
+            while (true)
+            {
+                //Inicia uma  Tarefa
+                try
+                {
+                    await Task.Delay(time, tcancel.Token);
+                }
+                catch (TaskCanceledException ex)
+                {
+                    //Verifica se a task foi cancelada 
+                    if (tcancel.IsCancellationRequested)
+                    {
+                        tcancel.Dispose();
+                        tcancel = new CancellationTokenSource();
+                        Thread.Sleep(2000); //Da um tempinho para parar a thread, dá só uma seguradinha fera                    
+                    }
+                }
+
+                CarregaDados();
+            }
+            
+        }
+
+        /// <summary>
+        /// Método responsável por carregar os dados na grid
+        /// </summary>
+        private void CarregaDados()
+        {
+           
+
+                using (IDbConnection conexao = ConnectionFactory.CriaConexao())
+                using (IDbCommand comando = conexao.CreateCommand())
+                {
+                   
+                    comando.CommandText = query;
+                    IDataReader leitor = comando.ExecuteReader();
+                    binding.DataSource = leitor;
+                    dgMonitor.DataSource = binding;
+                }
+            
+
+        }
+
+
+        private void FormataGrid(object sender, EventArgs e)
+        {
+            dgMonitor.Columns["Status"].Width = 120;
+            dgMonitor.Columns["Emissão"].Width = 120;
+            dgMonitor.Columns["Sequência"].Width = 120;
+            dgMonitor.Columns["Lote"].Width = 120;
+            dgMonitor.Columns["Produto"].Width = 150;
+            dgMonitor.Columns["Ficha Pallet"].Width = 120;
+            dgMonitor.Columns["OP"].Width = 150;
+            dgMonitor.Columns["Nota Fiscal"].Width = 120;
+            dgMonitor.Columns["Quantidade Lida"].Width = 180;
+            dgMonitor.Columns["Quantidade Apontada"].Width = 230;
+
+
+        }
+
+        private void formatagrid2(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+
+            if (dgMonitor.Rows[e.RowIndex].Cells["Status"].Value.ToString().Trim() == "DEVOLVIDO")
+            {                
+                dgMonitor.Rows[e.RowIndex].Cells["Status"].Style.BackColor = Color.OrangeRed;
+            }                  
+                               
+            if (dgMonitor.Rows[e.RowIndex].Cells["Status"].Value.ToString().Trim() == "EXPEDICAO")
+            {                  
+                dgMonitor.Rows[e.RowIndex].Cells["Status"].Style.BackColor = Color.DeepSkyBlue;
+            }                  
+                               
+            if (dgMonitor.Rows[e.RowIndex].Cells["Status"].Value.ToString().Trim() == "RECEBIDO")
+            {                  
+                dgMonitor.Rows[e.RowIndex].Cells["Status"].Style.BackColor = Color.GreenYellow;
+            }                  
+            if (dgMonitor.Rows[e.RowIndex].Cells["Status"].Value.ToString().Trim() == "TRANSITO")
+            {                  
+                dgMonitor.Rows[e.RowIndex].Cells["Status"].Style.BackColor = Color.Gold;
+            }
+
+
+            dgMonitor.Rows[e.RowIndex].Height = 50;
+
+            dgMonitor.Rows[e.RowIndex].Cells["Status"].Style.Font = oFontg;
+            dgMonitor.Rows[e.RowIndex].Cells["Emissão"].Style.Font = oFontg;
+            dgMonitor.Rows[e.RowIndex].Cells["Hora"].Style.Font = oFontg;
+            dgMonitor.Rows[e.RowIndex].Cells["Sequência"].Style.Font = oFontg;
+            dgMonitor.Rows[e.RowIndex].Cells["Produto"].Style.Font = oFontg;
+            dgMonitor.Rows[e.RowIndex].Cells["OP"].Style.Font = oFontg;
+            dgMonitor.Rows[e.RowIndex].Cells["Lote"].Style.Font = oFontg;
+            dgMonitor.Rows[e.RowIndex].Cells["Ficha Pallet"].Style.Font = oFontg;
+            dgMonitor.Rows[e.RowIndex].Cells["Carga"].Style.Font = oFontg;
+            dgMonitor.Rows[e.RowIndex].Cells["Nota Fiscal"].Style.Font = oFontg;
+            dgMonitor.Rows[e.RowIndex].Cells["Quantidade Lida"].Style.Font = oFontg;
+            dgMonitor.Rows[e.RowIndex].Cells["Quantidade Apontada"].Style.Font = oFontg;
         }
     }
+
+    
+
 }
+
---
+++ MonitorFichaPallet/MonitorFichaPallet/Helpers/ConnectionFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace MonitorFichaPallet.Helpers
+{
+    public class ConnectionFactory
+    {
+        public static IDbConnection CriaConexao()
+        {
+            var stringConexao = "Server = SRV-FINI-DBH; Database = P_DEV; User Id = SIGA; Password = SIGA";
+            IDbConnection conexao = new SqlConnection();
+            conexao.ConnectionString = stringConexao;//stringConexao.ConnectionString;
+            conexao.Open();
+            return conexao;
+        }
+    }
+}
